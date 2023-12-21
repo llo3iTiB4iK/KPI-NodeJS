62f315783db414427ab5bd930e3d11598708d008
llo3iTiB4iK <lyalyaivan2004@gmail.com>
2023-12-21 12:23:27 +0200

    added functionality to server

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const i = 0 + 3;
-console.log(i);
+import express from "express";
+const app = express();
+const port = 3000; // Оберіть порт, на якому буде працювати сервер
+
+// Маршрут головної сторінки
+app.get("/", (req, res) => {
+  res.send("Це головна сторінка!");
+});
+
+// Маршрут для /about
+app.get("/about", (req, res) => {
+  res.send('Це сторінка "Про нас".');
+});
+
+// Маршрут для /contact
+app.get("/contact", (req, res) => {
+  res.send('Це сторінка "Контакти".');
+});
+
+// Слухати запити на вибраному порту
+app.listen(port, () => {
+  console.log(`Сервер запущено на порті ${port}`);
+});
